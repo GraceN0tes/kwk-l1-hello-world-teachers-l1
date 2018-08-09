e0a028e2a7f2317a7d9a4bfc4aaab0fbe83d73d5
Grace Park <parkgra3@gmail.com>
2018-08-09 14:36:16 +0000

    automatically backed up by learn

---
+++ saumya1.rb
@@ -0,0 +1,40 @@
+def option_b
+  puts "Give me an adjective."
+  adjective1 = gets.chomp
+  puts "Give me another adjective."
+  adjective2 = gets.chomp
+  puts "Give me a celebrity."
+  celebrity1 = gets.chomp
+  puts "Give me another celebrity."
+  celebrity2 = gets.chomp
+  puts "Give me an animal."
+  animal1 = gets.chomp
+  puts "Give me a verb."
+  verb1 = gets.chomp
+  puts "Give me a noun."
+  noun1 = gets.chomp
+  puts "Give me another noun."
+  noun2 = gets.chomp
+  puts "Give me a body part."
+  bodypart1 = gets.chomp
+  puts "Give me an adverb."
+  adverb1 = gets.chomp
+  puts "Give me another adjective."
+  adjective3 = gets.chomp
+  puts "Give me another adverb."
+  adverb2 = gets.chomp
+  puts "Give me another adjective."
+  adjective4 = gets.chomp
+  puts "Give me another adjective."
+  adjective5 = gets.chomp
+  puts "Give me another body part."
+  bodypart2 = gets.chomp
+  puts "Give me another adjective."
+  adjective6 = gets.chomp
+  puts "Give me another noun."
+  noun3 = gets.chomp
+  puts "Give me another noun."
+  noun4 = gets.chomp
+  puts "Give me the name of someone in this room(male)."
+  male1 = gets.chomp
+  puts "'My Dream Man' should, first of all, be very #{adjective1} and #{adjective2}. He should have a physique like #{celebrity1}, a profile like #{celebrity2}, and the intelligence of a/an #{animal1}. He must be polite and must always remember to #{verb1} my #{noun1}, to tip his #{name2}, and to take my #{bodypart1} when crossing the street. He should move #{adverb1}, have a/an #{adjective3} voice, and should always dress #{adverb2}. I would also like him to be a/an #{adjective4} dancer, and when we are alone, he should whisper #{adjective5} nothings into my #{bodypart2} and hold my #{adjective6} #{noun3}. I know a/an #{noun4} is hard to find. In fact, the only one I can think of is #{male1}." 
