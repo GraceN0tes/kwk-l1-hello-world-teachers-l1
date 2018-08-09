53076a3093cd5703ba1e3763e7ef481ebc178fa6
Grace Park <parkgra3@gmail.com>
2018-08-09 15:54:35 +0000

    automatically backed up by learn

--- 1D.rb
+++ 1D.rb
@@ -4,8 +4,12 @@ one_direction = {"Zayn" => 25,
                 "Niall" => 24
 }
 
+#LEVEL 1 
 #How can you print out all the members of 1D and their ages like the following:
 # Zayn is 25 years old 
 # Liam is 24 years old
 # etc
+
+#LEVEL 2 How can you get the average age of the members of 1D?
+
 
