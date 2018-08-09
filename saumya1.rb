9ed0d16e2c6f0b4dbc528d1aebe1f801b05a8c8a
Grace Park <parkgra3@gmail.com>
2018-08-09 14:39:41 +0000

    automatically backed up by learn

--- saumya1.rb
+++ saumya1.rb
@@ -1,45 +1,4 @@
-def option_b
-  puts "Give me an adjective."
-  adjective1 = gets.chomp
-  puts "Give me another adjective."
-  adjective2 = gets.chomp
-  puts "Give me a celebrity."
-  celebrity1 = gets.chomp
-  puts "Give me another celebrity."
-  celebrity2 = gets.chomp
-  puts "Give me an animal."
-  animal1 = gets.chomp
-  puts "Give me a verb."
-  verb1 = gets.chomp
-  puts "Give me a noun."
-  noun1 = gets.chomp
-  puts "Give me another noun."
-  noun2 = gets.chomp
-  puts "Give me a body part."
-  bodypart1 = gets.chomp
-  puts "Give me an adverb."
-  adverb1 = gets.chomp
-  puts "Give me another adjective."
-  adjective3 = gets.chomp
-  puts "Give me another adverb."
-  adverb2 = gets.chomp
-  puts "Give me another adjective."
-  adjective4 = gets.chomp
-  puts "Give me another adjective."
-  adjective5 = gets.chomp
-  puts "Give me another body part."
-  bodypart2 = gets.chomp
-  puts "Give me another adjective."
-  adjective6 = gets.chomp
-  puts "Give me another noun."
-  noun3 = gets.chomp
-  puts "Give me another noun."
-  noun4 = gets.chomp
-  puts "Give me the name of someone in this room(male)."
-  male1 = gets.chomp
-  puts "'My Dream Man' should, first of all, be very #{adjective1} and #{adjective2}. He should have a physique like #{celebrity1}, a profile like #{celebrity2}, and the intelligence of a/an #{animal1}. He must be polite and must always remember to #{verb1} my #{noun1}, to tip his #{name2}, and to take my #{bodypart1} when crossing the street. He should move #{adverb1}, have a/an #{adjective3} voice, and should always dress #{adverb2}. I would also like him to be a/an #{adjective4} dancer, and when we are alone, he should whisper #{adjective5} nothings into my #{bodypart2} and hold my #{adjective6} #{noun3}. I know a/an #{noun4} is hard to find. In fact, the only one I can think of is #{male1}."
-  
-  def option_a
+def option_a
   puts "give me a verb that ends in 'ing'"
   verbing1 = gets.chomp
   puts "give me a holiday"
@@ -90,6 +49,50 @@ def option_b
   verbing2 = gets.chomp
   puts "So I was #{verbing1}! I'm a kid, I'm supposed to snoop, especially around #{holiday1} time! What #{person1} doesn't snoop? I just so happened to be in our #{place1}. Normally I'm not up there, it's  #{adjective1} and #{adjective2}. Plus there are #{pluralanimal1}, yuck! I couldn't help myself! There it was right by my little sister's old #{noun1}. Guess #{person2} didn't think I would #{verb1} it. I #{verbed1} it, it felt #{adjective2}. Should I #{verb2} it? Looking closer I saw some #{pluralnoun1}. I got even closer and #{pastverb1} a #{noun2}! The hidden #{noun3} was alive! A yelp came from the #{noun4}. I opened the #{noun5} and to my surprise a #{noun6} was inside! Guess who else was hidden in the #{place2}? #{person3} and #{person4} just knew I was #{verbing2}!"
 end
+  
+  
+  
+  def option_b
+  puts "Give me an adjective."
+  adjective1 = gets.chomp
+  puts "Give me another adjective."
+  adjective2 = gets.chomp
+  puts "Give me a celebrity."
+  celebrity1 = gets.chomp
+  puts "Give me another celebrity."
+  celebrity2 = gets.chomp
+  puts "Give me an animal."
+  animal1 = gets.chomp
+  puts "Give me a verb."
+  verb1 = gets.chomp
+  puts "Give me a noun."
+  noun1 = gets.chomp
+  puts "Give me another noun."
+  noun2 = gets.chomp
+  puts "Give me a body part."
+  bodypart1 = gets.chomp
+  puts "Give me an adverb."
+  adverb1 = gets.chomp
+  puts "Give me another adjective."
+  adjective3 = gets.chomp
+  puts "Give me another adverb."
+  adverb2 = gets.chomp
+  puts "Give me another adjective."
+  adjective4 = gets.chomp
+  puts "Give me another adjective."
+  adjective5 = gets.chomp
+  puts "Give me another body part."
+  bodypart2 = gets.chomp
+  puts "Give me another adjective."
+  adjective6 = gets.chomp
+  puts "Give me another noun."
+  noun3 = gets.chomp
+  puts "Give me another noun."
+  noun4 = gets.chomp
+  puts "Give me the name of someone in this room(male)."
+  male1 = gets.chomp
+  puts "'My Dream Man' should, first of all, be very #{adjective1} and #{adjective2}. He should have a physique like #{celebrity1}, a profile like #{celebrity2}, and the intelligence of a/an #{animal1}. He must be polite and must always remember to #{verb1} my #{noun1}, to tip his #{name2}, and to take my #{bodypart1} when crossing the street. He should move #{adverb1}, have a/an #{adjective3} voice, and should always dress #{adverb2}. I would also like him to be a/an #{adjective4} dancer, and when we are alone, he should whisper #{adjective5} nothings into my #{bodypart2} and hold my #{adjective6} #{noun3}. I know a/an #{noun4} is hard to find. In fact, the only one I can think of is #{male1}."
+  
   
   
 def mad_libs
