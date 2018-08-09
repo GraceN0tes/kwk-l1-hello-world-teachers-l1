18052bd275ba93d8b19aa99618a258558a8f56b0
Grace Park <parkgra3@gmail.com>
2018-08-09 15:52:27 +0000

    automatically backed up by learn

--- 1D.rb
+++ 1D.rb
@@ -4,4 +4,8 @@ one_direction = {"Zayn" => 25,
                 "Niall" => 24
 }
 
-#
+#How can you print out all the members of 1D and their ages like the following:
+# Zayn is 25 years old 
+# Liam is 24 years old
+# etc
+
