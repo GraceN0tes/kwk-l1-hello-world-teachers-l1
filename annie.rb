83345825fed9c1e5f5372df1cd01b96661987d82
Grace Park <parkgra3@gmail.com>
2018-08-09 14:49:46 +0000

    automatically backed up by learn

--- annie.rb
+++ annie.rb
@@ -1,36 +1,20 @@
-def option_a
-  puts "give me a verb ending in ing"
-  verb1 = gets.chomp
-  puts "give me a city"
-  city1 = gets.chomp
-  puts "give me the name of a celebrity"
-  celebrity1 = gets.chomp
-  puts "give me an adjective"
-  adjective1 = gets.chomp
-  puts "give me a verb in past tense"
-  pastverb1 = gets.chomp
-  puts "give me another city"
-  city2 = gets.chomp
-  puts "give me the name of another celebrity"
-  celebrity2 = gets.chomp
-  puts "give me another adjective"
-  adjective2 = gets.chomp
-  puts "give me a noun"
-  noun1 = gets.chomp
-  puts "give me a verb ending in ing"
-  verb2 = gets.chomp
-  puts "Once upon a time, I was #{verb1} in #{city1}, when my world came crashing down. There, walking right in front of me was the actual #{celebrity1}. I was shook. #{celebrity1} was looking #{adjective1} today. I had #{pastverb1} all day today, so this made my day so much better. I was sad because I knew I would have to leave #{city1} soon, so I couldn't spend the day with #{celebrity1}. Then, they suggested we go TOGETHER on their PRIVATE JET to #{city2}. I'M QUAKING. #{celebrity1} is soooo #{adjective2}. When we arrived in #{city2}, #{celebrity1} informed me that we would be staying with #{celebrity2}!!!! HOW IS MY LIFE SO #{adjective2}!!! To thank #{celebrity1} for literally saving my entire life, I got them #{noun1}. I love being generous, wow. Well, I guess I will spend the rest of eternity #{verb2} to go back in time."
-  
+def user_ml
+ puts "Give me a verb"
+ verb1 = gets.chomp
+ puts "Give me a noun"
+ noun1 = gets.chomp
+ puts "Give me another noun"
+ noun2 = gets.chomp
+ puts "Give me an adjective"
+ adjective1 = gets.chomp
+ puts "Give me a place"
+ place1 = gets.chomp
+ puts "Give me a verb"
+ verb2 = gets.chomp
+ puts "Give me an adjective"
+ adjective2 = gets.chomp
+
+ puts "Madison began #{verb1} becuase she was being chaesd by a #{noun1} and a #{noun2} she was so #{adjective1} and was confused as to why they were following her. Finally she found a #{place1} and hid there until she knew for a fact that they were gone. After that she was #{verb2} home to tell her family what crazy day she had. She bagan telling her parents the craxy story and they parents #{adjective2} to what Madison was tell them."
 end
-def mad_lib
-  puts "Heyyyyy, welcome to my super cool, super fun, super awesome mad lib. Hope you have fun. Pick option a, b, or c. But really pick a."
-  
-  option = gets.chomp
-  if option == "a"
-    puts option_a
-    elsif option == "b"
-    puts option_b
-  else puts options_c
-  end 
-end 
-mad_lib
+
+user_ml
