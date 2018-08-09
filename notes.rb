8eb67edd50d672d933038b0d849e0afe65332409
Grace Park <parkgra3@gmail.com>
2018-08-09 15:38:47 +0000

    automatically backed up by learn

---
+++ notes.rb
@@ -0,0 +1,11 @@
+NOTES: 
+
+ITERATION with Array
+.each method allows us to do something to each item in an Array
+numbers = [1, 2, 3, 4]
+numbers.each do |x|
+  puts x 
+end 
+#this outputs 1, 2, 3, 4
+
+new_array = []
